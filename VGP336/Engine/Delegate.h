d807767573d0ca50ebde212b9c8420e2fc1f5334
Tyler <Tyler.A.Staples@gmail.com>
2015-05-16 23:09:26 -0700

    Removed old delegate classes as the variadic version replaces them

--- VGP336/Engine/Delegate.h
+++ VGP336/Engine/Delegate.h
@@ -10,119 +10,25 @@
 //				http://blog.coldflake.com/posts/C++-delegates-on-steroids/
 //====================================================================================================
 
-//#define MAKE_DELEGATE(
-//Delegate<bool, s32> del = Delegate<bool, s32>::Make<TestApp, &TestApp::OnCameraLook>(this);
-//Delegate<bool, s32, s32, bool> del2 = Delegate<bool, s32, s32, bool>::Make<TestApp, &TestApp::OnPanCamera>(this);
+//====================================================================================================
+// Macros
+//====================================================================================================
+
+#define DELEGATE(PARAM_CALLBACK, PARAM_OWNER)\
+    (MakeDelegate(PARAM_CALLBACK).Bind<PARAM_CALLBACK>(PARAM_OWNER))
 
 //====================================================================================================
 // Class Declarations
 //====================================================================================================
 
-template <typename ReturnType>
-class Delegate0
-{
-public:
-	Delegate0()
-		: mInstance(nullptr)
-		, mInvoke(nullptr)
-	{}
-
-	ReturnType operator()() const
-	{
-		return (*mInvoke)(mInstance);
-	}
-
-	bool Empty() const
-	{
-		return mInstance == nullptr || mInvoke == nullptr;
-	}
-
-	void Clear()
-	{
-		mInstance = nullptr;
-		mInvoke = nullptr;
-	}
-	
-	template <class T, ReturnType (T::*MethodType)()>
-	static Delegate0 Make(T* instance)
-	{
-		Delegate0 d;
-		d.mInstance = instance;
-		d.mInvoke = &Invoke<T, MethodType>;
-		return d;
-	}
-
-private:
-	typedef ReturnType (*InvokeType)(void*);
-
-	void* mInstance;
-	InvokeType mInvoke;
-
-	template <class T, ReturnType (T::*MethodType)()>
-	static ReturnType Invoke(void* instance)
-	{
-		T* ptr = static_cast<T*>(instance);
-		return (ptr->*MethodType)();
-	}
-};
-
-//----------------------------------------------------------------------------------------------------
-
-template <class ReturnType, class ParamType>
-class Delegate1
-{
-public:
-	Delegate1()
-		: mInstance(nullptr)
-		, mInvoke(nullptr)
-	{}
-
-	ReturnType operator()(ParamType p1) const
-	{
-		return (*mInvoke)(mInstance, p1);
-	}
-
-	bool Empty() const
-	{
-		return mInstance == nullptr || mInvoke == nullptr;
-	}
-
-	void Clear()
-	{
-		mInstance = nullptr;
-		mInvoke = nullptr;
-	}
-	
-	template <class T, ReturnType (T::*MethodType)(ParamType)>
-	static Delegate1 Make(T* instance)
-	{
-		Delegate1 d;
-		d.mInstance = instance;
-		d.mInvoke = &Invoke<T, MethodType>;
-		return d;
-	}
-
-private:
-	typedef ReturnType (*InvokeType)(void*, ParamType);
-
-	void* mInstance;
-	InvokeType mInvoke;
-
-	template <class T, ReturnType (T::*MethodType)(ParamType)>
-	static ReturnType Invoke(void* instance, ParamType p1)
-	{
-		T* ptr = static_cast<T*>(instance);
-		return (ptr->*MethodType)(p1);
-	}
-};
-
 template <class ReturnType, typename... Params>
 class Delegate
 {
+	typedef ReturnType (*InvokeType)(void*, Params...);
 public:
-	Delegate()
-		: mInstance(nullptr)
-		, mInvoke(nullptr)
+	Delegate(void* callee, InvokeType callback)
+		: mInstance(callee)
+		, mInvoke(callback)
 	{}
 
 	ReturnType operator()(Params... p1) const
@@ -144,15 +50,11 @@ public:
 	template <class T, ReturnType (T::*MethodType)(Params...)>
 	static Delegate Make(T* instance)
 	{
-		Delegate d;
-		d.mInstance = instance;
-		d.mInvoke = &Invoke<T, MethodType>;
+		Delegate d(instance, &Invoke<T, MethodType>);
 		return d;
 	}
 
 private:
-	typedef ReturnType (*InvokeType)(void*, Params...);
-
 	void* mInstance;
 	InvokeType mInvoke;
 
@@ -163,5 +65,28 @@ private:
 		return (ptr->*MethodType)(p1...);
 	}
 };
+
+template <typename T, class ReturnType, typename... Params>
+struct DelegateMaker
+{
+    template <ReturnType (T::*MethodType)(Params...)>
+    static ReturnType Invoker(void* o, Params... p)
+    {
+        return (static_cast<T*>(o)->*MethodType)(p...);
+    }
+
+    template <ReturnType (T::*MethodType)(Params...)>
+    inline static Delegate<ReturnType, Params...> Bind(T* o)
+    {
+        return Delegate<ReturnType, Params...>(o, &DelegateMaker::Invoker<MethodType>);
+    }
+};
+
+template <typename T, typename ReturnType, typename... Params>
+DelegateMaker<T, ReturnType, Params...>
+MakeDelegate(ReturnType (T::*)(Params...))
+{
+    return DelegateMaker<T, ReturnType, Params...>();
+}
 
 #endif // #ifndef INCLUDED_ENGINE_DELEGATE_H
